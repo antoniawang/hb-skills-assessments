f39d451170e0ec58d90dc9789522f9f98775e103
Antonia Wang <antonia.t.wang@gmail.com>
2015-07-27 00:50:30 -0700

    finished further study

--- week4/skills.sql
+++ week4/skills.sql
@@ -136,14 +136,23 @@ GROUP BY brand_name HAVING Count(*) > 5;
 
 -- 2. Add the following rows to the Models table.
 
--- year    name       brand_name
+-- year    brand_name name -- CORRECTED THIS
 -- ----    ----       ----------
 -- 2015    Chevrolet  Malibu
 -- 2015    Subaru     Outback
 
+INSERT INTO Models (year, name, brand_name)
+VALUES (2015, 'Malibu', 'Chevrolet'),
+(2015, 'Outback', 'Subaru');
+
 -- 3. Write a SQL statement to crate a table called ``Awards`` 
 --    with columns ``name``, ``year``, and ``winner``. Choose 
 --    an appropriate datatype and nullability for each column.
+
+CREATE TABLE Awards(
+    name VARCHAR(20) NOT NULL,
+    year INTEGER(4) NOT NULL,
+    winner_model_id INTEGER(2));
 
 -- 4. Write a SQL statement that adds the following rows to the Awards table:
 
@@ -151,10 +160,20 @@ GROUP BY brand_name HAVING Count(*) > 5;
 --   ----                 ----      ---------------
 --   IIHS Safety Award    2015      # get the ``id`` of the 2015 Chevrolet Malibu
 --   IIHS Safety Award    2015      # get the ``id`` of the 2015 Subaru Outback
+UPDATE Awards SET name = 'IIHS Safety Award';
+
+INSERT INTO Awards (name, year, winner_model_id)
+  VALUES ('IIHS Safety Award', 2015, (SELECT Models.id FROM Models WHERE Models.name = 'Malibu')),
+         ('IIHS Safety Award', 2015, (SELECT Models.id FROM Models WHERE Models.name = 'Outback'));
+
+
 
 -- 5. Using a subquery, select only the *name* of any model whose 
 -- year is the same year that *any* brand was founded.
 
+SELECT Models.name FROM Models
+JOIN Brands ON (brand_name = Brands.name)
+WHERE year IN (SELECT founded FROM Brands GROUP BY founded);
 
 
 
